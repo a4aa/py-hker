313c8660af42511a05d53d4a05682edfdd69cf42
a4aa <79127903+a4aa@users.noreply.github.com>
2021-08-10 12:55:20 +0530

    Create webjs.js

---
+++ webjs.js
@@ -0,0 +1,516 @@
+const cookie = {
+    "cookie": JSON.load("cookie.json")
+}
+const imports = [
+    "JSON",
+    "dwm",
+    "nmp",
+    "node_js",
+    "imp",
+    "py-my-js"
+]
+
+imports.download().catch(function (err) {console.error(err + '\n')});
+cookie.set();
+
+
+class Element {
+    constructor() {
+        show.InnerHTML = Element
+    }
+
+    create(name) {
+        c = new Element 
+        c.constructor(name)
+    }
+}
+
+function get(inp, cb) {
+    data = [];
+    data.push(inp);
+
+    index = void '';
+
+    collect(index, cb);
+
+    document.lastElementChild(data, function() {
+        req('web-brow', index, function() {
+            document.write(cb);
+        })
+    })
+}
+
+function request(req, res, cb) {
+    get(req, function() {
+        getElementById('web-brow').InnerHTML = res.setInterval(() => {
+            delete('web-brow');
+            makeModules('web-browser');
+        }, cb);
+    })
+}
+
+function makeModules(mn) {
+    mn.InnerHTML = function() {
+        data = [];
+        data.push(mn);
+
+        get(mn, function() {
+            Element.create(mn);
+        })
+    }
+}
+
+
+// n = true;
+// if (n == true) {
+//     nt = function(arg) {
+//         document.write(arg)
+//     }
+
+//     nt(request('input', ' ', function() {
+//         get('input', function() {
+//             loaction = get('input', '@route/')
+//         })
+//     }))
+// }
+// test.
+
+function globalize(arg, global) {
+    if (arg === undefined || global === undefined) {
+        return document.error('Arg or Global values are undifined')
+    } else {
+        if (arg === global) {
+            return document.error('arg cannot be global')
+        } else {
+            global(arg, function() {
+                get(arg, function() {
+                    loaction = get(global, '@route/global');
+                    if (location == true){
+                        return true;
+                    } else {
+                        return false;
+                    }
+                })
+            })
+        }
+    }
+}
+
+function respond(req, res, n) {
+    if (req == res || req.method == 'GET') {
+        return n;
+    } else {
+        get(res, function() {
+            get(n, function() {
+                loaction = get(n, '@route/n');
+                if (loaction == true) {
+                    return true;
+                } else {
+                    return false;
+                }
+            })
+        })
+    }
+}
+
+function route(path, file, get, post_ins, d = null) {
+    if (path === file === get === post_ins === d) {
+        return false;
+    } else {
+        globalize(path, function() {
+            get(path, file);
+        }).catch(function(err) {
+            console.error(err);
+        })
+
+        if (path == '@route') {
+            console.error('Invalid @route');
+        } else {
+            if (file == 'back-slash') {
+                console.error('Invalid back-slash');
+            } else {
+                if (get === path) {
+                    console.error('Invalid syntax');
+                    system.exit();
+                } else {
+                    if (d == '/data/collection/logs/d.log') {
+                        console.error('Invalid data');
+                        system.exit;
+                    } else {
+                        broweser = [];
+                        broweser.push(d);
+
+                        if (broweser[0] == 'xS%j54D') {
+                            location = path;
+                            location.reload();
+                            if (broweser[0] == 'xS%j54D') {
+                                browser[0] == 'xS%j54D0';
+                            }
+                        } else {
+                            if (location == '/@route/browser/@path') {
+                                location = '@route/browser';
+                            } else {
+                                server.start('flask', path)
+                                if (web_broweser.close()) {
+                                    server.close('*');
+                                    browser[0] = '';
+                                    system.exit();
+                                } else {
+                                    server.hold(d, function() {
+                                        if (d == empty()) {
+                                            d.fill(upcoming_data(), data.pass())
+                                            d.send(file = '/data/collection/logs/data.log');
+                                            if (d == error()) {
+                                                error().catch(function (err) {
+                                                    console.error(err + '\n');  
+                                                    system.exit();
+                                                    cookie.clear();
+                                                })
+                                            } else {
+                                                globalize(data);
+                                                globalize(cookie);
+                                                data.pass();
+                                                borwser.cookie_control() = true;
+                                                browser.Element().catch(function (err) {console.error(err + '\n');});
+                                                if (data.pass() == true) {
+                                                    if (change.code()) {
+                                                        update();
+                                                    } else {
+                                                        change_of_code() = change.code();
+                                                        get(change_of_code(), '@app/server/index.js' && '@app/server/index.js.edit.log');
+                                                    }
+                                                } else {
+                                                    get(null, function () {
+                                                        data.pass() = '@3RO#/D7H$/comp/bin/index@app/@app/app/server/#HID@hid/bin.conf.json';
+                                                    }); 
+                                                    if (code.edit() == by_view()) {
+                                                        code.save(previous_code());
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }).catch(function (err) {
+                                        x = err;
+                                        document.write(err);
+                                        console.error(err);
+                                    })
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+function load_db(path, db, file_name, code, bin_browser_cache, cache, d) {
+    if (path === db === file_name === code === bin_browser_cache === cache === d) {
+        return false;
+    } else {
+        if (db === 'database.db') {
+            console.error('!');
+        } else {
+            if (code === 'load') {
+                return true;
+            } else {
+                if (bin_browser_cache == null || bin_browser_cache == '') {
+                    console.error('cache cannot be null or empty');
+                } else {
+                    if (d == 'sqlX07&sqlZ03') {
+                        location = reload();
+                        if (d == 'sqlX07&sqlZ03') {
+                            console.error('!sql informat');
+                            system.exit();
+                        }
+                    } else {
+                        load(db, file_name, bin_browser_cache, d, cache, '@$/.db');
+                    }
+                }
+            }
+        }
+    }
+}
+
+function load(a,b,c,d,e,f,g) {
+    if (error() != true) {
+        a.Element();
+        b.InnerHTML = a.Inner = [];
+
+        c = [];
+        d = [];
+        e = [];
+        f = [];
+        g = [];
+
+        JS.load_file(a,b,c,d,e,f,g);
+    } else {
+        system.exit();
+    }
+}
+
+function exit() {
+    location = 'about:blank';
+    location = '';
+}
+
+any = async()
+
+function website_start(locatio = 'about:blank', domain = '.Net', db, add_files = []) {
+    if (route(any,any,any,any,any) == true) {
+        if (locatio == 'about:blank') {
+            console.warn('Website may not appear.');
+        } else {
+            if (domain == '.Net') {
+                console.warn('Domain may be .net');
+                location == locatio + domain;
+            } else {
+                location = locatio + domain;
+                data_base(db);
+                if (add_files != '') {
+                    add_new_files_in_web(add_files);
+                } else {
+                    console.warn('Only html file may be loaded.')
+
+                }
+            }
+        }
+    } else {
+        console.error('route not defined');
+    }
+}
+
+function website_login(admin_html, na_html, logined_html) {
+    iusr = Element.add(type='Input_box', id = 'usrn', context = 'Enter Username');
+    ipdw = Element.add(type='Input_box', id = 'pawd', context = 'Enter Password');
+    b = Element.add(type='IButton', id = 'ispd', context = 'Create a new account')
+
+
+    if (iusr == 'AdminA4ar' && ipdw == 'AdminLoginCreds') {
+        Element.starup(admin_html);
+    } else {
+        if (b == isPressed()) {
+            Element.startup(na_html);
+        } else if (iusr != '' && ipdw != '') {
+            if (Database.seacrh(iusr, ipdw) == true) {
+                Element.startup(logined_html);
+            } else {
+                Element.show('Error: Invalid Username or Password');
+            }
+        }
+    }
+}
+
+function get_downloaded_files_button(fle = []) {
+    downloaded_files = [fle];
+
+    if (downloaded_files == false) {
+        console.error('!sql!df!');
+    } else {
+        Element.add(type='IButton', id = 'dwnd', context = 'Download', files = downloaded_files);
+    }
+}
+
+function get_downloaded_files_url(fle = []) {
+    downloaded_files = [fle];
+    if (downloaded_files == false) {
+        console.error('!sql!df!');
+    } else {
+        Element.add(type='Iurl', id = 'dwnd', context = 'Download', files = downloaded_files, color = 'Blue');
+    }
+}
+
+function url(url) {
+    location = url;
+}
+
+function button(id) {
+    Element.add(type = 'IButton', id = id);
+}
+
+function input(context, id, text) {
+    Element.add(type = 'input_box', id = id, context = context, text = text);
+}
+
+function page(url, htmlc) {
+    location = url; 
+    file(htmlc);
+}
+
+function get_t(type, fts) {
+    if (type == 'ip' || type == 'url') {
+        if (type == 'ip') {
+            File.send(file = fts, content = target);
+            xt = null;
+            target = get(IP, function() {
+                get(IP, function() {
+                    xt = IP;
+                });
+            });
+        } else if (type == 'url') {
+            File.send(file = fts, content = target);
+            xu = null;
+            targt = get(URL, function() {
+                get(URL, function() {
+                    xu = URL;
+                });
+            });
+        } else {
+            console.warn("Invalid Web ststus. error: INVALID_get_t()_FUNCTION_IN_LOOP");
+            system.exit();
+        }
+    } else {
+        console.error('!Invalid !type:' + type);
+    }
+}
+
+function comment(txt) {
+    if (txt == '') {
+        Comment.add__txt() = txt;
+    } else {
+        console.error("Need some text to create a comment");
+    }
+}
+
+function newlib(name, code) {
+    var lib_ = name
+    document.write(code + name + '\n');
+    if (lib_ == "webjs") {
+        console.error("Thats the lib you are using! invalid name!")
+    }
+}
+
+function check_errs(url, catch_var) {
+    if (url = '@route/web-page.html') {
+        console.error('web-page');
+    } else {
+        var err = Error();
+        for (var i = 0; i < 11; 1++) {
+            if (catch_var == err) {
+                Error().fix(type = err, fix_perms = 'admin.prs');
+            } else {
+                console.log('No errors found!');
+            }
+        }
+    }
+}
+
+function anti_ddos() {
+    if (pings < 1000) {
+        get(pings, function () {
+            get(acoount_pings, function () {
+                acc = account_pings;
+                acc.exit()
+            })
+        })
+    }
+} 
+
+function anti_hack() {
+    // console.error('hack found')
+
+    if (process.env.hack.Database == True || "prhd.y") {
+        console.warn('prhd.y: hack found.');
+        get(process.env.hack.Database, function () {
+            get(process.env.hack.account, function() {
+                var hack_acc = process.env.hack.account;
+                hack_acc.exit();
+                console.log("Theat is removed from system.");
+                if (hack_acc.login()) {
+                    document.write("Sorry, hacking is not allowed.");
+                    console.log("Hacker tried to acsess");
+                }
+            }) 
+        })
+    } else {
+        var d = process.env.hack;
+        if (d) {
+            console.log("You are safe to continue... no hacking is found.");
+            d.exit();
+        }
+    }
+}
+
+function close_website(route, db) {
+    if (route == '@route') {
+        system.exit();
+    } else {
+        route = null;
+        route() = null;
+        db() = null;
+        db = null;
+        website.exit();
+    }
+}
+
+function run() {
+    route = true;
+    route() = true;
+    db() = true;
+    db = true;
+    website.start();
+}
+
+function fun() {
+    code.input() = 'function code() { // code } ';
+    code.save();
+    if (run()) {
+        code.run()
+    } else {
+        cose.save();
+    }
+}
+
+function input(input, options) {
+    if (typeof input === 'string') {
+        if (typeof options === 'string') {
+            var target = inputbox(input, options);
+            if (typeof target === 'string') {
+                var target_ans = string(ans)
+            } else if (typeof target === 'integer') {
+                get(ans, function () {
+                    get(ans, function() {
+                        var ans = target_ans
+                    })
+                })
+                var target_ans = int(ans)
+                return ans;
+            }
+        } else {
+            console.error('!str-opts')
+        }
+    } else if (typeof input === 'integer') {
+        console.error('Please enter a valid type of input');
+    }
+}
+
+function output() {
+    if (input === undefined) {
+        console.error('Please enter a valid type of input : !UND :');
+    } else {
+        get(input() ==  output(), function() {
+            get(output() == output(a), function() {
+                return a;
+            })
+        }) 
+    }
+}
+
+
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+
+/*
+functions: Webjs;
+type: Library;
+Copyright AarZGaming.
+*/
